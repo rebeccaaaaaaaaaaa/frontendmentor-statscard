42c8bf54ba1f6aa12643314645af57bdb3b69e75
rebecca <rebeccaignacio1996@gmail.com>
2021-12-24 19:33:50 -0300

    ultimas atualizações finalizadas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import style from './App.module.css';
+import headerImage from './images/image-header-desktop.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section>
+        <div className={style.card}>
+          <div className={style.card__text}> 
+            <h1 className={style.card__text__principal}> Get <span className={style.card__text__principal__span}>insights</span> that help your business grow.</h1>
+            <p className={style.card__text__secondary}> Discover the benefits of data analytics and make better decisions regarding revenue, 
+                customer experience, and overall efficiency. </p>
+
+              <div className={style.card__text__counter}>
+                <div className='card__text--counter-item'>
+                  <h2> 10K+ </h2>
+                  <p className={style.card__text__counter_item_descrip}> Companies </p>
+                </div>
+                <div className='card__text--counter-item'>
+                  <h2> 314 </h2>
+                  <p className={style.card__text__counter_item_descrip}> Templates </p>
+                </div>
+                <div className='card__text--counter-item'>
+                  <h2> 12M+ </h2>
+                  <p className={style.card__text__counter_item_descrip}> Queries </p>
+                </div>
+              </div>
+          </div>
+
+          <div className={style.card__image}>
+            <img src={headerImage} alt='People smilling in a office'/>
+            <div className={style.card__image_overlay}></div>
+          </div>
+       
+        </div>
+      </section>
+    </main>
   );
 }
 
